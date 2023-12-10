c63609ef1d52da74b1dfae019590fc8e4678edd0
Nazim Ahmedov <61702781+nazim117@users.noreply.github.com>
2023-12-10 18:16:29 +0100

    Add files via upload

---
+++ BankLoan_Skeleton_netcoreapp6.0/BankLoan/Models/MortgageLoan.cs
@@ -0,0 +1,13 @@
+﻿namespace BankLoan.Models
+{
+    public class MortgageLoan : Loan
+    {
+        private const int interestRate = 3;
+        private const double amount = 50000;
+
+        public MortgageLoan()
+            : base(interestRate, amount)
+        {
+        }
+    }
+}
---
+++ BankLoan_Skeleton_netcoreapp6.0/BankLoan/Models/StudentLoan.cs
@@ -0,0 +1,14 @@
+﻿namespace BankLoan.Models
+{
+    public class StudentLoan : Loan
+    {
+        private const int interestRate = 1;
+        private const double amount = 10000;
+
+        public StudentLoan()
+            : base(interestRate, amount)
+        {
+
+        }
+    }
+}
